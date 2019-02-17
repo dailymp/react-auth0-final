390636ea8d9cce5bf10202daff26af7c5f49940f
Day MP <dailymp@gmail.com>
2019-02-17 13:03:24 +0100

    Testing mongo db conecctions and serverless with Stitch

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+const {
+  Stitch,
+  RemoteMongoClient,
+  AnonymousCredential
+} = require('mongodb-stitch-browser-sdk');
 
 class Home extends Component {
   render() {
@@ -10,7 +15,7 @@ class Home extends Component {
         {isAuthenticated() ? (
           <Link to="/profile">View profile</Link>
         ) : (
-          <button onClick={login}>Log In</button>
+          <button onClick={login1}>Log In</button>
         )}
       </div>
     );
@@ -18,3 +23,34 @@ class Home extends Component {
 }
 
 export default Home;
+
+
+
+const client = Stitch.initializeDefaultAppClient('demo-kflbk');
+
+const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('test');
+
+const login1 = () => {
+  client.auth.loginWithCredential(new AnonymousCredential()).then(user =>
+  db.collection('users').updateOne({
+    owner_id: client.auth.user.id
+  }, {
+    $set: {
+      number: 77
+    }
+  }, {
+    upsert: true
+  })
+).then(() =>
+  db.collection('users').find({
+    owner_id: client.auth.user.id
+  }, {
+    limit: 100
+  }).asArray()
+).then(docs => {
+  console.log("Found docs", docs)
+  console.log("[MongoDB Stitch] Connected to Stitch")
+}).catch(err => {
+  console.error(err)
+});
+}
